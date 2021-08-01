ce865522063d0caf50974ea53bcc8de03975552c
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-08-01 15:22:54 +0530

    test

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
+@app.route('/predict_api', methods=['GET', 'POST'])
+def list_post():
+    json_body = request.get_json()
+    predictions = 2 * json_body[0]   
+    predictions = list(predictions)
+    return jsonify(results = predictions)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
